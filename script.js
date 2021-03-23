25f7cec4588af8430f14a4ea32a1160e6e07ab3a
Gabriel Nunes <51537551+ogabriell@users.noreply.github.com>
2021-03-22 21:21:03 -0300

    final

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+text = ['border-top-left-radius:','border-top-right-radius:','border-bottom-left-radius:','border-bottom-right-radius:','width:','height:'];
+
 function tl(){
     var val = document.getElementById('tl');
     var tl = document.getElementById('border');
     if(val.value >= 0){
         tl.style.borderTopLeftRadius = val.value+'px';
+        msg(0,val.value,'ttl');
     }
 }
 function tr(){
@@ -10,6 +13,7 @@ function tr(){
     var tr = document.getElementById('border');
     if(val.value >= 0){
         tr.style.borderTopRightRadius = val.value+'px';
+        msg(1,val.value,'ttr');
     }
 }
 function bl(){
@@ -17,6 +21,7 @@ function bl(){
     var bl = document.getElementById('border');
     if(val.value >= 0){
         bl.style.borderBottomLeftRadius = val.value+'px';
+        msg(2,val.value,'tbl');
     }
 }
 function br(){
@@ -24,6 +29,49 @@ function br(){
     var br = document.getElementById('border');
     if(val.value >= 0){
         br.style.borderBottomRightRadius = val.value+'px';
+        msg(3,val.value,'tbr');
     }
 }
-
+function w(){
+    var val = document.getElementById('w');
+    var w = document.getElementById('border');
+    if(val.value <= 1000){
+        w.style.width= val.value+'px';
+        msg(4,val.value,'tw');
+    }
+}
+function h(){
+    var val = document.getElementById('h');
+    var h = document.getElementById('border');
+    if(val.value <= 1000){
+        h.style.height= val.value+'px';
+        msg(5,val.value,'th');
+    }
+}
+function msg(cod,num,nm){
+    var write = document.getElementById(nm);
+    write.innerHTML = text[cod]+num+'px';
+}
+function copy(){
+    var val = document.getElementById('ttl');
+    var copy = val.textContent;
+    var val = document.getElementById('ttr');
+    var copy = copy+`
+`+val.textContent;
+    var val = document.getElementById('tbl');
+    var copy = copy+`
+`+val.textContent;
+    var val = document.getElementById('tbr');
+    var copy = copy+`
+`+val.textContent;
+    var val = document.getElementById('tw');
+    var copy = copy+`
+`+val.textContent;
+    var val = document.getElementById('th');
+    var copy = copy+`
+`+val.textContent;
+    // console.log(copy);
+    navigator.clipboard.writeText(copy)
+    document.execCommand("copy");
+    alert('Copiado')
+}
